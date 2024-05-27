a742adf00999b53d0a55269573580a059baf765c
jddevoffical <jddevoffical@gmail.com>
2024-05-27 14:12:45 +0530

    update changes

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/home', (req, res) => {
     res.send('Homepage')
 })
 
+app.get('/', (req, res) => {
+    res.json({message: "welcome to node js"})
+})
+
 app.listen(8080, () => {
     console.log(`The is app listening port 8080`)
 })
